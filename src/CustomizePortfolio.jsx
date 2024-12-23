152d3901f480c6f2505d0f431e0a927e5d00a45b
amirmousav1 <socialam1rmousav1@gmail.com>
2024-12-23 19:34:18 +0330

    updated resume url

--- src/CustomizePortfolio.jsx
+++ src/CustomizePortfolio.jsx
@@ -91,7 +91,8 @@ export const myInformations = {
   linkedinUrl: "https://www.linkedin.com/in/am1rmousav1",
   githubUrl: "https://github.com/amirmousav1",
   telegramUrl: "https://t.me/shomabeguamir",
-  resumeUrl: "#",
+  resumeUrl:
+    "https://drive.google.com/file/d/1kjNjW2dt1i3Mf9tsWOGurjOGnYl9JYb9/view?usp=sharing",
   aboutTitle: "I am a front-end developer based in Shiraz",
   aboutMe:
     "Hi there! I'm a 20-year-old frontend developer from Shiraz, studying IT. <br /> I pay close attention to details and always try to deliver the best results. Solving problems excites me, and I'm always motivated to learn new things. <br /> When I'm not coding, I enjoy playing chess, watching football, and catching up on movies and TV shows. <br /> Feel free to reach out if you'd like to collaborate on something great!",
